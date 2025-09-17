a3117d6798920d16bec1e14fa5298da176a3e89e
krsticzoran <zorankrstic81@gmail.com>
2025-09-17 13:33:19 +0200

    feat: highlight active navigation link with JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector("#mobile-menu");
 const body = document.body;
@@ -6,4 +7,16 @@ hamburger.addEventListener("click", function () {
   this.classList.toggle("active");
   mobileMenu.classList.toggle("show");
   body.classList.toggle("menu-open");
+});
+
+//nav active link
+const currentPath = window.location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach((link) => {
+  const linkPath = link.getAttribute("href");
+  if (linkPath === currentPath) {
+    link.classList.add("active-link");
+  }
 });
